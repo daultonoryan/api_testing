3dc2c59e9860a3f221ef768f4ac89717b0b27cda
daultonryan <daultonoryan@gmail.com>
2018-09-16 02:10:21 -0400

    adjusted for no picture api and added water and light output controls

--- greenpi.py
+++ greenpi.py
@@ -4,13 +4,13 @@ import os
 import argparse
 import time
 import requests
+import datetime
 
 
 GPIO.setmode(GPIO.BCM)
-
+headers = {"Authorization": "Token d906b53efa966d6dfa24224621801f25db6f637b"}
 
 def sensor_data_post(sensor_data):
-    headers = {"Authorization": "Token d906b53efa966d6dfa24224621801f25db6f637b"}
     try:
         r = requests.post("http://127.0.0.1:8000/sensors/", headers=headers, data=sensor_data)
         print(r)
@@ -35,11 +35,24 @@ def get_temp_hum(pin):
     return temp, hum
 
 
+def water_plant(pin):
+    try:
+        r = requests.get("http://127.0.0.1:8000/outputs/", headers=headers)
+        print(r)
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, 1)
+        time.sleep(int(r["daily_water"]) / 5)
+        GPIO.output(pin, 0)
+    except Exception as e:
+        print("get failure ", e)
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-temp_pin", help="input pin for temperature humidity sensor", type=int, required=True)
     parser.add_argument("-soil_moisture_pin", help="input pin for soil moisture", type=int, required=True)
     parser.add_argument("-water_level_pin", help="input pin for water level", type=int, required=True)
+    parser.add_argument("-watering", help="input pin for watering", type=int, required=True)
+    parser.add_argument("-light", help="input pin for light", type=int, required=True)
     args = parser.parse_args()
     count = 0
     while True:
@@ -48,16 +61,32 @@ def main():
         print("moisture: %s" % soil)
         if soil == 1:
             print("please refill tank")
-        os.system("raspistill -o rpi_imgs/" + str(count) + ".jpg")
+        #os.system("raspistill -o rpi_imgs/" + str(count) + ".jpg")
         sensor_data = {"temp": temp,
                        "unit_id": os.environ["DEVICE_ID"],
                        "humidity": hum,
                        "soil_moisture": soil,
                        "light_status": True,
-                       "snapshot": open("rpi_imgs/" + str(count) + ".jpg", "r")}
-
-	time.sleep(5)
-	count += 1
+                       #"snapshot": open("rpi_imgs/" + str(count) + ".jpg", "r")
+                        }
+        now = datetime.datetime.now()
+        if now.hour == 1 & now.minute == 20:
+            print("doing the thing")
+            water_plant(args.watering)
+            time.sleep(60)
+        try:
+            r = requests.get("http://127.0.0.1:8000/outputs/", headers=headers)
+            print(r)
+            if 0 <= now.hour <= int(r["daily_light"] - 1):
+                GPIO.setup(args.light, GPIO.OUT)
+                GPIO.output(args.light, 1)
+            else:
+                GPIO.setup(args.light, GPIO.OUT)
+                GPIO.output(args.light, 0)
+        except Exception as e:
+            print("get failure ", e)
+        time.sleep(30)
+        count += 1
 
 
 if __name__ == "__main__":
